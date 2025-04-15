73b249088fcbe0de1ffb41f8e9993a886ded3bb1
anyuka007 <anna.popova.de@gmail.com>
2025-04-15 21:24:00 +0200

    Refactor ServiceCard component for improved layout and styling of descriptions

--- src/app/components/ServiceCard/ServiceCard.tsx
+++ src/app/components/ServiceCard/ServiceCard.tsx
@@ -7,28 +7,29 @@ type ServiceCardProps = {
     description: string[];
 }
 
-const serviceListElement = "pl-3 my-1 flex gap-1"
 const ServiceCard = ({ name, description }: ServiceCardProps) => {
     return (
-        <div className="service-card lg:relative w-[90%] h-[400px] mx-auto lg:w-[47%]  mb-5 rounded-[8px] shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2] lg:rounded-none lg:shadow-none" > 
-            <div className="w-full h-1/2 lg:w-[60%] lg:h-[65%] rounded-[8px] lg:shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2]">
-            <img 
-                    src="/images/hero-1.jpg" 
-                    alt="Service image" 
-                    className="w-full h-full object-cover rounded-t-[8px] lg:rounded-[8px]" 
-                    loading="lazy" 
+        <div className="service-card relative w-[90%] h-[400px] mx-auto md:w-[47%]  mb-5 rounded-[8px] shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2] md:rounded-none md:shadow-none" >
+            <div className="w-full h-1/2 md:w-[85%] lg:w-[75%] xl:w-[65%] md:h-[65%] rounded-[8px] md:shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2]">
+                <img
+                    src="/images/hero-1.jpg"
+                    alt="Service image"
+                    className="w-full h-full object-cover rounded-t-[8px] md:rounded-[8px]"
+                    loading="lazy"
                 />
-                
+
             </div>
-            <div className="w-full h-1/2 lg:absolute  lg:bottom-0 lg:right-0 lg:w-[60%] lg:h-[65%]  bg-[#B671AD] rounded-b-[8px] lg:rounded-[8px] lg:shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2]">
-                <h2 className="md:text-xl font-bold text-[#A2BC8D] text-center">{name}</h2>
+            <div className="w-full h-1/2 px-3 md:absolute md:w-[85%]  md:bottom-0 md:right-0 lg:w-[75%] xl:w-[65%] md:h-[65%] bg-[#B671AD] rounded-b-[8px] md:rounded-[8px] md:shadow-[8px_8px_24px_#bebebe,-8px_-8px_24px_#e2e2e2] flex flex-col gap-2">
+                <h2 className="text-lg lg:text-xl leading-tight font-bold text-[#A2BC8D] text-center pt-2">{name}</h2>
                 <div className="flex flex-col">
                     {description.map((el, index) => (
-                        <div className={serviceListElement} key={index}><Truck className="w-5 h-5 flex-shrink-0"/>{el}</div>
+                        <div className="my-1 flex gap-1" key={index}>
+                            <Truck className="w-5 h-5 flex-shrink-0" /><p className="text-sm lg:text-base leading-tight lg:leading-snug">{el}</p>
+                        </div>
                     ))}
                 </div>
-                </div>
-            
+            </div>
+
         </div>
         /*Without shadows */
 
@@ -48,7 +49,7 @@ const ServiceCard = ({ name, description }: ServiceCardProps) => {
                 </div>
             
         </div> */
-        
+
     );
 }
 export default ServiceCard;
